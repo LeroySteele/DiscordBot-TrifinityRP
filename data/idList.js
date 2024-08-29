3fc63c70d84144fa090ceea424c00f2c7619ad1b
Leroy Steele <60223453+LeroySteele@users.noreply.github.com>
2024-08-29 14:50:29 +0200

    Add files via upload

---
+++ data/idList.js
@@ -0,0 +1,105 @@
+let idList = {
+    //Role ID's 
+    governmentRole : "1175999805357559815",
+    onLeaveRole : "1228702820589375530",
+    
+    commissionerRole : "1175999805365944395",
+    chiefOfLSPDRole : "1175999805365944393",
+    blaineCountySheriffRole : "1176004000563331072",
+
+    deputyChiefRole : "1175999805365944392",
+    deputySheriffRole : "1176005278429368391",
+    highCommandRole : "1175999805357559817",
+
+    captainRole : "1175999805365944391",
+    BCSOMajorRole : "1176006386186997810",
+
+    LSPDLieutenantRole : "1175999805344989251",
+    BCSOLieutenantRole : "1175999805344989250",
+
+    LSPDStaffSergeant : "1211082388528963585",
+    BCSOStaffSergeant : "1233775782661193839",
+
+    LSPDSergeantRole : "1175999805344989249",
+    BCSOSergeantRole : "1175999805344989248",
+    supervisorRole : "1176013090198921216",
+
+    LSPDCorporalRole : "1175999805332389944",
+    BCSOCorporalRole : "1175999805332389943",
+
+    LSPDSeniorOfficerRole : "1175999805332389941",
+    BCSOSeniorDeputyRole : "1175999805332389939",
+
+    LSPDOfficerRole : "1175999805319811142",
+    BCSODeputyRole : "1175999805319811141",
+
+    LSPDRecruitRole : "1175999805319811136",
+    BCSORecruitRole : "1175999805319811137",
+
+    LSPDRole : "1176004865827278868",
+    BCSORole : "1176005069808877692",
+
+    CitizenRole : "1175999805210771507",
+    alienRole : "1233774687859904552",
+
+    ftoLeadRole : "1181922403106959492",
+    interceptorLeadRole : "1175999805277868126",
+    interceptorTrainerRole : "1176935353500831774",
+    trtLeadRole : "1175999805307240687",
+    airSupportLeadRole : "1175999805240115316",
+
+    ftoRole : "1175999805344989247",
+    trtRole : "1176602121534197841",
+    class2 : "1176870880228421642",
+    interceptorRole : "1175999805277868124",
+    air1Role : "1175999805240115313",
+    
+
+    //category ID's
+    policeApplicationsCategory : "1176601040909504533",
+    publicAffairsCategory : "1175999805961551902",
+    citizenComplaintsCategory : "1176041979843444787",
+    applicationsAndPermitsCategory : "1186318688631148575", 
+    policeDashboardCategory : "1175999808536842422",
+    policeComplaintsCategory : "1176600688285991043",
+    enrollIntoTheAcademyCategory : "1176016009744896120",
+    divisionTestApplicationsCategory : "1176857495990444032",
+    ticketLogsCategory : "1228857798964547654",
+    refundCategory : "1238684077557547130",
+
+    //channel ID's
+    joinPDChannel : "1233814102082846852",
+    applyNowChannel : "1176035743798399027",
+    complaintsChannel : "1176041483699228747",
+    PermitsChannel : "1186317667582349363",
+    officerComplaintsChannel : "1175999808536842429",
+    divisionTestsChannel : "1176016319360024686",
+    LSPDClothingChannel : "1176534053265092629",
+    BCSOClothingChannel : "1176534089046695966",
+    botLogsChannel : "1233815275879534612",
+
+    ftoOfficersChannel : "1205166036274511932",
+    trtOfficersChannel : "1233815275879534612",
+    class2OfficersChannel : "1233815275879534612",
+    interceptorOfficersChannel : "1233815275879534612",
+    air1OfficersChannel : "1233815275879534612",
+    marineOfficersChannel : "1233815275879534612",
+    refundChannel : "1238684702613835887",
+    warrantChannel : "1269784212991508523",
+    cadetReportChannel : "1271166350080872499",
+    suggestionChannel : "1269784434802950165",
+
+    //transcripts
+    policeApplicationsTrans : "1269795596500340746",
+    citizenComplaintsTrans : "1269795729782603836",
+    policeComplaintsTrans : "1269795826977345547",
+    PermitsTrans : "1269795908351164437",
+    divisionTestsTrans : "1269796000185450567",
+    refundTrans : "1269796203508404296",
+
+    //Reaction Icons
+    tickReaction : "✅",
+    crossReaction : "❌",
+
+}
+module.exports = Object.freeze(idList);
---
+++ data/ids.js
@@ -0,0 +1,10 @@
+let idList = {
+    TOKEN : "TOKEN_FROM_DiscordDeveloperPortal",
+    CLIENT_ID : "BOT_ID",
+    GUILD_ID : "SERVER_ID",
+
+    serviceAccountKeyFile : "./google.json",
+    sheetId : "GOOGLE_SHEET_LINK",
+    range : "A:H",
+};
+module.exports = Object.freeze(idList); 
